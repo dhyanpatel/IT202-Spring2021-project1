0bea639d07c9012ae70758bf19f1be0a3b268bf1
Dhyan Patel <dhyanpp@gmail.com>
2021-02-26 15:16:19 -0600

    Create App bar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item href="#">Home</b-nav-item>
+
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+name: "AppBar"
+}
+</script>
+
+<style scoped>
+
+</style>
